b8fba32f4e4baad4ecfd04276c2a76db3776213c
K_Sav <savelevkirill20@gmail.com>
2023-08-02 15:39:47 +0300

    Home_Work_6

--- advertisements/app_lesson_4/models.py
+++ advertisements/app_lesson_4/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Advertisements(models.Model):
+    title = models.CharField('заголовок', max_length=100)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('торг', help_text='Возможен торг или нет')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'advertisements(id = {self.id}, title = {self.title}, price = {self.price})'
+
+    class Meta:
+        db_table = 'advertisements'
